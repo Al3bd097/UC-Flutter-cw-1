7aa498bcf264840e2b5d065aae19cd20344df3c7
Al3bd097 <al3bdf@gmail.com>
2022-11-23 19:20:37 +0300

    .

--- Cw 1/lecture1.dart
+++ Cw 1/lecture1.dart
@@ -1,7 +1,12 @@
 void main() {
-  int temp = 80;
-    print("${(temp - 32) * 5 / 9}");
-
-
-    
+  double height = 1.7;
+  double weight = 90;
+  double bmi = (weight) / (height * height);
+  if (bmi >= 24.9) {
+    print("Overweight");
+  } else if (bmi < 24.9 && bmi >= 18.6) {
+    print("Normal");
+  } else {
+    print("underWeight");
+  }
   }
