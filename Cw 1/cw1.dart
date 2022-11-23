2d3dce820f0a9916c4a50188fdda9052383be700
Al3bd097 <al3bdf@gmail.com>
2022-11-23 19:25:38 +0300

    .

---
+++ Cw 1/cw1.dart
@@ -0,0 +1,11 @@
+void main() {
+ String firstName = "Abdullah";
+ String lastName = "Albannay";
+ int age = 18;
+ String hobby = "Programming";
+  print("First name: $firstName");
+  print("Last name: $lastName");
+  print("I'm $age , my hobby is $hobby");
+  
+  
+  }
